3cfdfe823bb01c0d8dd7e902a2d63d46cf0bb8c0
Byron Norvel <bjnorvel1980@gmail.com>
2022-09-21 14:23:38 -0500

    Message

---
+++ main.py
@@ -0,0 +1,35 @@
+destinations = [ "New York City" , "San Francisco" , "Chicago" ]
+print(destinations)
+
+random_destination = "San Francisco"
+print(random_destination)
+
+restaurants = [ "Chinese" , "Mexican" , "French"]
+print(restaurants)
+random_restaurant= "French"
+print(random_restaurant)
+
+transportation = ["Rental car", "Uber", "Train"]
+print(transportation) 
+random_transportation = "Train"
+print(random_transportation)
+
+entertainment = ["Bars", "Theater", "Sightseeing"]
+print(entertainment)
+random_entertainment = "Sightseeing"
+print(random_entertainment)
+
+random_entertainment= "Sightseeing"
+print("Sightseeing")
+if random_entertainment == "Theater":
+    print("Theaters are great")
+elif random_entertainment != "Sightseeing":
+    print("This will do for now")
+else:
+    print("This is not sightseeing")
+
+
+random_selections = ["Sightseeing", "Restaurants", "Theater"]
+for random_selection in random_selections:
+    print(random_selection)
+
